a1591d96e535c5e751e757725d0773ef6c9761d4
nithyarg <nithyaasbsc@gmail.com>
2024-07-19 07:04:45 +0530

    31m

--- nithya_portfoli/components/Header.jsx
+++ nithya_portfoli/components/Header.jsx
@@ -6,6 +6,7 @@ import React, {useState, useEffect} from 'react';
 import Logo from './Logo';
 import ThemeToggler from "./ThemeToggler";
 import Nav from './Nav';
+import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
@@ -13,10 +14,14 @@ const Header = () => {
      <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
         <Logo/>
-        <div>
+        <div className='flex items-center gap-x-6'>
           {/* nav */}
-          <Nav/>
+        <Nav containerStyles='hidden xl:flex gap-x-B items-center'/>
          <ThemeToggler/>
+         {/* mobile nav */}
+         <div className='xl:hidden'>
+          <MobileNav />
+         </div>
         </div>
       </div>
       </div>
--- nithya_portfoli/components/Nav.jsx
+++ nithya_portfoli/components/Nav.jsx
@@ -1,7 +1,22 @@
-const Nav = () => {
-  return 
-    <div>Nav</div>;
-  
+
+// link (next js)
+import Link from "next/link";
+
+const links =[
+  {path:'/',name:'home'},
+  {path:'/projects',name:'my projects'},
+  {path:'/contact', name:'contanct'},
+];
+
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles}) => {
+  return (
+    <nav className={`${containerStyles}`}>
+      {links.map((link,index)=>{
+        return <Link href={link.path}>{link.name}</Link>;
+      })}
+    </nav>
+);
 };
 
 export default Nav;
