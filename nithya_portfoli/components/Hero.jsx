b72a586424eb443befc105dcc500aa6213c816f3
nithyarg <nithyaasbsc@gmail.com>
2024-07-20 21:00:05 +0530

    48m

--- nithya_portfoli/components/Hero.jsx
+++ nithya_portfoli/components/Hero.jsx
@@ -17,16 +17,17 @@ import Socials from './Socials';
 
 const Hero = () => {
   return (
-    <section>
+    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-pink-100'>
         <div className='container mx-auto'>
-            <div>
+            <div className='flex justify-between gap-x-8'>
                {/* text */}
              <div>text</div>
               {/* image */}
               <div>image</div>   
             </div>
             {/* icon */}
-            <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12' >
+            <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12
+            animate-bounce'>
                 <RiArrowDownSLine className='text-3xl text-primary'/>
             </div>
         </div>
--- nithya_portfoli/components/Logo.jsx
+++ nithya_portfoli/components/Logo.jsx
@@ -2,13 +2,16 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Logo = () => {
-  return 
-  <Link href='/'>
-    <Image src ='/logo.svg' width={54} height={54} priority
-    alt=''/>
+  return (
+    <Link href="/">
+      <Image
+        src="/logo.svg"
+        width={54}
+        height={54}
+        priority alt=''
+      />
+    </Link>
+  )
+}
 
-  </Link>;
-  
-};
-
-export default Logo;
+export default Logo
