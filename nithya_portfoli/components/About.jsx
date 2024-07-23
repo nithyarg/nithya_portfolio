9ab8c2b46ed25393d1f5d1b90bae845b76f022aa
nithyarg <nithyaasbsc@gmail.com>
2024-07-23 22:46:04 +0530

    1h:58m error

--- nithya_portfoli/components/About.jsx
+++ nithya_portfoli/components/About.jsx
@@ -87,11 +87,103 @@ const qualificationData = [
 ];
 
 
-const SkillData = [];
+const skillData = [
+  {
+    title: 'skills',
+    data:[
+        {
+            name:'HTML,CSS',
+        },
+        {
+            name:'Front-end- Development',
+        },
+        {
+            name:'Javascript, PHP',
+        },
+        {
+            name:'Back-end Development',
+        },
+    ],
+  },
+  {
+    title: 'tools',
+    data:[
+        {
+            imgPath:'/about/vscode.svg',
+        },
+        {
+            imgPath:'/about/figma.svg',
+        },
+        {
+            imgPath:'/about/notion.svg',
+        },
+        {
+            imgPath:'/about/wordpress.svg',
+        },
+    ],
+  },
+];
 
 const About = () => {
+   const getData = (arr, title) =>{
+    return arr.find((item) => item.title === title);
+   };
+  
   return (
-    <div>About</div>
+    <section className="xl:h-[860px] pb-12 xl:py-24">
+        <div className="container mx-auto">
+           <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
+           <div className="flex flex-col xl:flex-row">
+            {/* image */}
+            <div className="hidden xl:flex flex-1 relative">
+                <DevImg containerStyles='bg_about_shape_light
+                dark:bg-about_shape_dark w-[505px] h-[505px]
+                bg-no-repeat relative' imgSrc='/about/developer.png'/>
+            </div>
+            {/* tabs */}
+            <div className="flex-1">
+                <Tabs defaultValue='personal'>
+                    <TabsList className='w-full grid xl:grid-cols-3 xl:max-w-[520px]
+                    xl:border dark:border-none'>
+                        <TabsTrigger className='w-[162px] xl:w-auto' value='personal'>Personal Info</TabsTrigger>
+                        <TabsTrigger className='w-[162px] xl:w-auto' value='qualifications'>Qualifications</TabsTrigger>
+                        <TabsTrigger className='w-[162px] xl:w-auto' value='skills'>Skills</TabsTrigger>
+                    </TabsList>
+                    {/* tabs content */}
+                    <div className="text-lg mt-12 xl:mt-8">
+                        {/* personal */}
+                        <TabsContent value='personal'>
+                            <div className="text-center xl:text-left">
+                                <h3 className="h3 mb-4">
+                                    Unmatched Service Quality for Over 10 Years
+                                </h3>
+                                <p className="subtitle max-w-xl mx-auto xl:mx-0">
+                                    I specialize in crafting intuitive websites with
+                                    cuttin-edge technology, delivering dynamic and engaging
+                                    user experiences.
+                                </p>
+                                {/* icons */}
+                                <div>
+                                    {infoData.map((item, index) => {
+                                        return( 
+                                            <div key={index}>
+                                            <div>{item.icon}</div>
+                                            <div>{item.text}</div>
+                                        </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </TabsContent>
+                        <TabsContent value='qualifications'>qualifications info</TabsContent>
+                        <TabsContent value='skills'>skills info</TabsContent>
+                    </div>
+                </Tabs>
+            </div>
+           </div>
+        </div>
+        
+    </section>
   );
 }
 
