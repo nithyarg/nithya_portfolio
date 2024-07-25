27f86e95ebc2f8436296dbdc342e5e84c0adbc0d
nithyarg <nithyaasbsc@gmail.com>
2024-07-25 22:16:48 +0530

    2h:23m

--- nithya_portfoli/components/About.jsx
+++ nithya_portfoli/components/About.jsx
@@ -271,7 +271,53 @@ const About = () => {
                         </TabsContent>
                         {/* skills */}
                         <TabsContent value='skills'>
-                            
+                            <div className="text-center xl:text-left">
+                                <h3 className="h3 mb-8">What I Use Everyday</h3>
+                                {/* skills */}
+                                <div className="mb-16">
+                                    <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                                    <div className="border-b border-border mb-4"></div>
+                                    {/* Skill list */}
+                                    <div>
+                                        {getData(skillData, 'skills').data.map(
+                                            (item, index) => {
+                                                const {name} = item;
+                                                return(
+                                                    <div className="w-2/4 text-center xl:text-left mx-auto xl:mx-0" 
+                                                     key={index}
+                                                    >
+                                                        <div className="font-medium">{name}</div>
+                                                    </div>
+                                                );
+                                            }
+                                        )}
+                                    </div>
+                                </div>
+                                {/*tools */}
+                                <div>
+                                    <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                                        Tools
+                                    </h4>
+                                    <div className="border-b border-border mb-4"></div>
+                                    {/* tool list */}
+                                    <div className="flex gap-x-8 justify-center center xl:justify-start">
+                                        {getData(skillData,'tools').data.map((item,index)=>{
+                                          const{imgPath} = item;
+                                        return(
+                                            <div key={index}>
+                                            <Image src={imgPath} 
+                                            width={48}
+                                            height={48}
+                                            alt=''
+                                            priority
+                                            />
+                                            </div>
+                                        );
+
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
                         </TabsContent>
                     </div>
                 </Tabs>
