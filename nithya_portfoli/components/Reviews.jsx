8564ab2329bf433d1d4496ff18d6800ad949d72b
nithyarg <nithyaasbsc@gmail.com>
2024-07-28 21:10:16 +0530

    3h.11m

--- nithya_portfoli/components/Reviews.jsx
+++ nithya_portfoli/components/Reviews.jsx
@@ -28,14 +28,81 @@ const reviewsData=[
         avatar:'/reviews/avatar-1.png',
         name: 'Richard Thompson',
         job: 'Chef',
-        review: '',
-    }
-]
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      avatar:'/reviews/avatar-2.png',
+      name: 'Evelyn Anderson',
+      job: 'Interior Designer',
+      review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+   {
+     avatar:'/reviews/avatar-3.png',
+     name: 'John Doe',
+     job: 'Game Dev',
+     review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+   },
+   {
+     avatar:'/reviews/avatar-4.png',
+     name: 'Emily Smith',
+     job: 'Therapist',
+     review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+   },
+   {
+   
+    avatar:'/reviews/avatar-5.png',
+    name: 'Oliver Taylor ',
+    job: 'Engineer',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    avatar:'/reviews/avatar-6.png',
+    name: 'Mason Wilson ',
+    job: 'Video Editor',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+];
 
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+   <section className='mb-12 xl:mb-32'>
+     <div className='container mx-auto'>
+      <h2 className='section-title mb-12 text-center mx-auto'>Reviews</h2>
+      {/* slider */}
+      <Swiper 
+       slidesPerView={1}
+       breakpoints={{
+        640:{slidesPerView:2},
+        1400:{slidesPerView: 3},
+      }}
+      spaceBetween = {30}
+      modules={[Pagination]}
+      pagination={{
+        clickable: true,
+      }}
+       className='h-[350px]'
+      >
+        {reviewsData.map((person,index) =>{
+          return(
+            <SwiperSlide key={index}>
+              <Card className='bg-tertiary dark:bg-secondary/40 p-8 min-h-[300px]'>
+                 <CardHeader className='p-0 mb-10'>
+                  <div>
+                    {/* image */}
+                    <Image/>
+                  </div>
+                 </CardHeader>
+                 <CardDescription className='text-lg text-muted-foreground'>
+                  {person.review}
+                  </CardDescription>
+                </Card>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+     </div>
+   </section>
   );
 };
 
